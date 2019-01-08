f5a0b1643cf53da6870c3de334f7f0e6297689ad
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2019-01-08 12:38:31 -0600

    index instagram section completed

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,13 +1,19 @@
 var blogCardImage = document.getElementsByClassName('--js-blog-card-image');
-
-console.log(blogCardImage);
+var instagramImage = document.getElementsByClassName('--js-instagram-image');
 
 for (var i = 0; i < blogCardImage.length; i++) {
   var height = blogCardImage[i].naturalHeight;
   var width  = blogCardImage[i].naturalWidth;
-  console.log(height, width);
   if (height > width) {
     blogCardImage[i].classList.add('long');
+  }
+}
+
+for (var i = 0; i < instagramImage.length; i++) {
+  var height = instagramImage[i].naturalHeight;
+  var width  = instagramImage[i].naturalWidth;
+  if (height > width) {
+    instagramImage[i].classList.add('long');
   }
 }
 
